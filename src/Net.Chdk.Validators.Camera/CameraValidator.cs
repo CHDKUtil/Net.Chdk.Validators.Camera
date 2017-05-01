4d6a1fa300cee40a11e0a0dc13fc4a0ca702e5b4
Dmitry Shechtman <damnian@gmail.com>
2017-05-02 02:33:03 +0300

    Add solution

---
+++ src/Net.Chdk.Validators.Camera/CameraValidator.cs
@@ -0,0 +1,55 @@
+﻿using Net.Chdk.Model.Camera;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net.Chdk.Validators.Camera
+{
+    sealed class CameraValidator : IValidator<CameraInfo>
+    {
+        public void Validate(CameraInfo camera, string basePath)
+        {
+            if (camera == null)
+                throw new ArgumentNullException(nameof(camera));
+
+            Validate(camera.Version);
+            Validate(camera.Base);
+            Validate(camera.Canon);
+        }
+
+        private static void Validate(Version version)
+        {
+            if (version == null)
+                throw new ValidationException("Null version");
+
+            if (version.Major < 1 || version.Minor < 0)
+                throw new ValidationException("Invalid version");
+        }
+
+        private static void Validate(BaseInfo @base)
+        {
+            if (@base == null)
+                throw new ValidationException("Null base");
+
+            if (string.IsNullOrEmpty(@base.Make))
+                throw new ValidationException("Missing make");
+
+            if (string.IsNullOrEmpty(@base.Model))
+                throw new ValidationException("Missing model");
+        }
+
+        private static void Validate(CanonInfo canon)
+        {
+            if (canon == null)
+                throw new ValidationException("Null canon");
+
+            if (canon.ModelId < 0x100000)
+                throw new ValidationException("Invalid modelId");
+
+            if (canon.FirmwareRevision < 0x1000000)
+                throw new ValidationException("Invalid firmwareRevision");
+
+            if ((canon.FirmwareRevision & 0xffff) < 0x100)
+                throw new ValidationException("Invalid firmwareRevision");
+        }
+    }
+}
---
+++ src/Net.Chdk.Validators.Camera/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Net.Chdk.Model.Camera;
+
+namespace Net.Chdk.Validators.Camera
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCameraValidator(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<IValidator<CameraInfo>, CameraValidator>();
+        }
+    }
+}
